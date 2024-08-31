805609cab902db93e8dc2f9a75404d7a5a70ee90
andrey <andkor0414@gmail.com>
2024-08-31 10:50:17 +0300

    c#

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Console.Write("Введите число > ");           // Вывод в консоль приглашения для ввода
+// string inputValue;                          // Объявление переменной, в которую будем сохранять пользоватльеский ввод
+// inputValue = Console.ReadLine();           // Присваивание переменной из консоли (ввод пользователя)
+// int value = Convert.ToInt32(inputValue);  // Конвертация в целое введенной строки
+
+// int square = value * value;             // Вычисление квадрата
+
+// // Вывод на экран
+// System.Console.WriteLine("Квадрат числа " + value + " равен " + square);
+// System.Console.WriteLine($"Квадрат числа {value} равен {square}");
+    
+// Console.WriteLine("Введите первое число:");
+//         int value1 = Convert.ToInt32(Console.ReadLine());
+
+//         Console.WriteLine("Введите второе число:");
+//         int value2 = Convert.ToInt32(Console.ReadLine());
+
+//         if (value1 > value2)
+//         {
+//             Console.WriteLine($"{value1} больше, чем value2}");
+//         }
+//         else if (value2 > value1)
+//         {
+//             Console.WriteLine($"{value2} больше, чем {value1}");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"{value1} и {value2} равны");
+//         }
+   
+// Console.WriteLine("Введите первое число:");
+//     int value1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число:");
+//         int value2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третие число:");
+//      int value3 = Convert.ToInt32(Console.ReadLine());
+
+    
+// Console.WriteLine("Введите три числа:");
+
+       
+//         int num1 = int.Parse(Console.ReadLine());
+//         int num2 = int.Parse(Console.ReadLine());
+//         int num3 = int.Parse(Console.ReadLine());
+
+//         int max = Math.Max(num1, Math.Max(num2, num3));
+
+//         Console.WriteLine($"Максимальное число: {max}");
